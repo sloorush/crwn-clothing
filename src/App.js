7ea5eba948ff4061e7367bd3e058fc4cebd2ae49
rush <aarush.bhatt@gmail.com>
2020-04-19 17:34:03 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
+import {Switch, Route} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component'
 
 import './App.css';
 
 function App() {
   return (
-    <div >
-      <Homepage /> 
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/hats' component={hats}/>
+
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem=({title, imageUrl,size})=>(
-    <div className={`${size} menu-item`}>
+const MenuItem=({title, imageUrl,size, history, linkUrl, match})=>(
+    <div className={`${size} menu-item`} onclick{()=> history.push(`${match.url}${linkUrl}`)}>
         <div style={{backgroundImage:`url(${imageUrl})`}} className='background-image' />
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
@@ -12,4 +13,4 @@ const MenuItem=({title, imageUrl,size})=>(
 </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
