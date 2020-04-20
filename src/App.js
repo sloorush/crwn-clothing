1fc0178f536e58051d72d0bca92f9186903dc234
rush <aarush.bhatt@gmail.com>
2020-04-20 13:48:14 +0530

    added signinsignup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
+import  SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage=()=>(
+    <div class='sign-in-and-sign-up'>
+    Sign In
+    </div>
+)
+
+export default SignInAndSignUpPage
