c5f39ba6ebaf6bb522f0cb92e26e21a52fa418fe
rush <aarush.bhatt@gmail.com>
2020-04-20 13:08:43 +0530

    added shop, collection preview, collection item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
