ce0d3a297f195cfa085d097ac12c69f6f0200959
rush <aarush.bhatt@gmail.com>
2020-04-21 10:03:10 +0530

    integrate oAuth

--- src/App.js
+++ src/App.js
@@ -5,19 +5,44 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
 import  SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
+import {auth} from './firebase/firebase.utils';
+
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Homepage}/>
-        <Route exact path='/shop' component={ShopPage}/>
-        <Route exact path='/signin' component={SignInAndSignUpPage}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state={
+      currentUser:null
+    }
+  }
+
+  unsubscribeFromAuth=null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth=auth.onAuthStateChanged(user => {
+      this.setState({currentUser:user});
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepage}/>
+          <Route exact path='/shop' component={ShopPage}/>
+          <Route exact path='/signin' component={SignInAndSignUpPage}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
