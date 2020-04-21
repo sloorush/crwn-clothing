c1a360f6426060e7e6a757d262988b80a9ed7c0c
rush <aarush.bhatt@gmail.com>
2020-04-22 00:04:28 +0530

    connected state to current user in db

--- src/App.js
+++ src/App.js
@@ -11,36 +11,52 @@ import './App.css';
 
 class App extends React.Component {
   constructor(){
-    super();
+	super();
 
-    this.state={
-      currentUser:null
-    };
+	this.state={
+	  currentUser:null
+	};
   }
 
   unsubscribeFromAuth=null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
-    })
+	this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+	  if (userAuth){
+		const userRef= await createUserProfileDocument(userAuth);
+		
+		userRef.onSnapshot(snapShot=>{
+			this.setState({
+				currentUser:{
+					id:snapShot.id,
+					...snapShot.data()
+				}
+			},()=>{
+				console.log(this.state)
+			});
+		});
+	  }
+	  else{
+		  this.setState({currentUser:userAuth});
+	  }
+	})
   }
 
   componentWillUnmount(){
-    this.unsubscribeFromAuth();
+	this.unsubscribeFromAuth();
   }
 
   render(){
-    return (
-      <div>
-        <Header currentUser={this.state.currentUser} />
-        <Switch>
-          <Route exact path='/' component={Homepage}/>
-          <Route exact path='/shop' component={ShopPage}/>
-          <Route exact path='/signin' component={SignInAndSignUpPage}/>
-        </Switch>
-      </div>
-    );
+	return (
+	  <div>
+		<Header currentUser={this.state.currentUser} />
+		<Switch>
+		  <Route exact path='/' component={Homepage}/>
+		  <Route exact path='/shop' component={ShopPage}/>
+		  <Route exact path='/signin' component={SignInAndSignUpPage}/>
+		</Switch>
+	  </div>
+	);
   }
 }
 
