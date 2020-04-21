4feb4f558c1b2aa428703121bd099c8c864af6e7
rush <aarush.bhatt@gmail.com>
2020-04-22 00:24:03 +0530

    added createUserWithEmailAndPassword

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import {auth , createUserProfileDocument} from '../../firebase/firebase.utils';
+
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component{
+    constructor(){
+        super();
+
+        this.state={
+            displayName:'',
+            email:'',
+            password:'',
+            confirmPassword:''
+        }
+    }
+
+    handleSubmit = async event=>{
+        event.preventDefault();
+        const {displayName, email, password, confirmPassword}=this.state;
+
+        if(password!== confirmPassword){
+            alert("passwords don't match");
+            return;
+        }
+        try{
+            const {user}=await auth.createUserWithEmailAndPassword(email, password);
+
+            await createUserProfileDocument(user, {displayName});
+
+            this.setState({
+                displayName:'',
+                email:'',
+                password:'',
+                confirmPassword:''
+            });
+        } catch(error){
+            console.error(error)
+        }
+    };
+
+    handleChange=event=>{
+        const {name, value}=event.target;
+        this.setState({[name]:value});
+    }
+
+    render() {
+        const {displayName, email, password, confirmPassword}=this.state;
+        return (
+            <div className='sign-up'>
+                <h2 className='title'>I do not have an account</h2>
+                <span>Sign up with email and password</span>
+                <form className='sign-up-form' onSubmit={this.handleSubmit}>
+                    <FormInput type='text' name='displayName' value={displayName} label='DisplayName' required />
+                    <FormInput type='email' name='email' value={email} label='Email' required />
+                    <FormInput type='password' name='password' value={password} label='password' required />
+                    <FormInput type='password' name='confirmPassword' value={confirmPassword} label='Confirm password' required />
+                    <CustomButton type='submit'>Sign up</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignUp
