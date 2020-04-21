899940223062581013b8ae18cb39808ae3631adc
rush <aarush.bhatt@gmail.com>
2020-04-21 23:30:48 +0530

    added oauth or firebase db

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
 import  SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -15,15 +15,14 @@ class App extends React.Component {
 
     this.state={
       currentUser:null
-    }
+    };
   }
 
   unsubscribeFromAuth=null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
     })
   }
 
