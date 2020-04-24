082b2b495b3f685ad697407608afe7db0410f333
r-ush <aarush.bhatt@gmail.com>
2020-04-24 16:28:56 +0530

    amend typo

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@ import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
-import { app } from 'firebase';
 
 const middleware=[logger];
 
