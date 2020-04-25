8c0cbc26174ad09da16bfe208be58923df2f7ed3
r-ush <aarush.bhatt@gmail.com>
2020-04-25 12:58:09 +0530

    added cart-component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import {auth} from '../../firebase/firebase.utils'
+import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component'
 
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import './header.styles.scss';
@@ -13,14 +14,15 @@ const Header=({currentUser})=>(
                 <Logo className='logo' />
         </Link>
         <div className='options'>
-        <Link className='option' to="shop">SHOP</Link>
-        <Link className='option' to="shop">CONTACT</Link>
-        {
-            currentUser ?
-            <div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div>
-            :
-            <Link className='option' to='/signin'>SIGN IN</Link>
-        }
+            <Link className='option' to="shop">SHOP</Link>
+            <Link className='option' to="shop">CONTACT</Link>
+            {
+                currentUser ?
+                <div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div>
+                :
+                <Link className='option' to='/signin'>SIGN IN</Link>
+            }
+            <CartIcon/>
         </div>
     </div>
 );
