809fdcbd453efaa70e169227103bba4485bda8f4
r-ush <aarush.bhatt@gmail.com>
2020-04-25 16:11:03 +0530

    added cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import './header.styles.scss';
 
-const Header=({currentUser})=>(
+const Header=({currentUser,hidden})=>(
     <div className='header'>
         <Link className='logo-container' to="/">
                 <Logo className='logo' />
@@ -25,12 +25,13 @@ const Header=({currentUser})=>(
             }
             <CartIcon/>
         </div>
-        <CartDropdown />
+        {hidden? null : <CartDropdown />}
     </div>
 );
 
-const mapStateToProps= state =>({
-    currentUser:state.user.currentUser
-})
+const mapStateToProps= ({user:{currentUser},cart:{hidden}}) =>({
+    currentUser,
+    hidden
+});
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,5 @@
+const CaseActionTypes={
+    TOGGLE_CART_HIDDEN:'TOGGLE_CART_HIDDEN'
+}
+
+export default  CaseActionTypes
